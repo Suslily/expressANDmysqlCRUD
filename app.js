5ad9993f1e07d616934b6b9b22bcec5b5f38e3ea
RyoIshizuka <>
2022-02-17 21:39:27 +0900

    crud操作

--- app.js
+++ app.js
@@ -4,36 +4,23 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const port = 3000
-
-
-
 const mysql = require('mysql2');
 
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
+
+// mysqlログイン情報
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Merutomo1',
-  database: 'express_db'
-});
+	host: 'localhost',
+	user: 'root',
+	password: 'test',
+	database: 'express_db'
+  });
 
 
-con.connect(function(err) {
-	if (err) throw err;
-	console.log('Connected');
-});
-
+// 現在テーブルに格納されているユーザー一覧
 app.get('/', (req, res) => {
-	const sql = "select * from users";
-	con.query(sql, function (err, result, fields) {  
-	if (err) throw err;
-  res.sendFile(path.join(__dirname, 'html/form.html'))
-	});
-});
-
-app.get('/list', (req, res) => {
 	const sql = "select * from users";
 	con.query(sql, function (err, result, fields) {  
 	if (err) throw err;
@@ -41,6 +28,19 @@ app.get('/list', (req, res) => {
 	});
 });
 
+app.post('/', (req, res) => {
+	const sql = "INSERT INTO users SET ?"
+	con.query(sql,req.body,function(err, result, fields){
+		if (err) throw err;
+		console.log(result);
+		res.redirect('/');
+	});
+});
+// 追加用のフォーム
+app.get('/create', (req, res) => 
+	res.sendFile(path.join(__dirname, 'html/form.html')))
+
+// すでに格納されているデータ編集用
 app.get('/edit/:id',(req,res)=>{
 	const sql = "SELECT * FROM users WHERE id = ?";
 	con.query(sql,[req.params.id],function (err, result, fields) {  
@@ -48,6 +48,16 @@ app.get('/edit/:id',(req,res)=>{
 		res.render('edit',{user : result});
 		});
 });
+//すでに格納されてるデータ更新用
+app.post('/update/:id',(req,res)=>{
+	const sql = "UPDATE users SET ? WHERE id = " + req.params.id;
+	con.query(sql,req.body,function (err, result, fields) {  
+		if (err) throw err;
+		console.log(result);
+		res.redirect('/');
+		});
+});
+//すでに格納されてるデータ削除用
 app.get('/delete/:id',(req,res)=>{
 	const sql = "DELETE FROM users WHERE id = ?";
 	con.query(sql,[req.params.id],function(err,result,fields){
@@ -57,13 +67,5 @@ app.get('/delete/:id',(req,res)=>{
 	})
 });
 
-app.post('/update/:id',(req,res)=>{
-	const sql = "UPDATE users SET ? WHERE id = " + req.params.id;
-	con.query(sql,req.body,function (err, result, fields) {  
-		if (err) throw err;
-		console.log(result);
-		res.redirect('/');
-		});
-});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
